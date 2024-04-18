3c83d92a38a88bdf413d0b40b0379227489339dc
keyurjivani <keyurjivani07rw@gmail.com>
2024-04-18 22:49:58 +0530

    commit

--- 18-4 lec 3/Find_your_friend.js
+++ 18-4 lec 3/Find_your_friend.js
@@ -1,15 +1,15 @@
 let n = 21;
 
 if (n < 13) {
-    console.log("Paresh is 1 kms away");
+    console.log("your frd is 1 kms away");
 }
 else if ((n >= 13) || (n < 18)) {
-    console.log("Paresh is 5 kms away");
+    console.log("your frd is 5 kms away");
 }
 else if ((n >= 18) || (n < 30)) {
-    console.log("Paresh is 10 kms away");
+    console.log("your frd is 10 kms away");
 }
 else {
-    console.log("Paresh can have friends from anywhere");
+    console.log("you can have friends from anywhere");
 
 }
