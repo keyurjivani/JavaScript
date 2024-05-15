26ade39da23f86780e3d7c978af80ba052f915e1
keyurjivani <keyurjivani07rw@gmail.com>
2024-05-15 19:10:35 +0530

    exam

--- 13-5 lec 9 table/index.js
+++ 13-5 lec 9 table/index.js
@@ -1,34 +1,63 @@
-let array = []
+let Array = [];
+
+const handledelete = (index) =>{
+    Array.splice(index,1)
+    Ui();
+}
 
 const Ui = () => {
-    document.getElementById("tbody").innerHTML = ""
+    document.getElementById("tbody").innerHTML = " ";
+    Array.map ((item,i)=>{
 
-    array.map((ele)=>{
-        let tr = document.createElement("tr");
-        
-        let td = document.createElement("td")
-        td.innerHTML = ele.UserName
+        let tr = document.createElement("tr")
 
-        tr.append(td)
+        let td1 = document.createElement("td")
+        td1.innerHTML = item.UserName;
+
+        let td2 = document.createElement("td");
+        td2.innerHTML = item.email;
+
+        let td3 = document.createElement("td");
+        td3.innerHTML = item.Dob;
+
+        let td4 = document.createElement("td");
+        td4.innerHTML = item.City;
+
+        let td5 = document.createElement("td");
+        td5.innerHTML = item.Password;
+
+        let td6 = document.createElement("td");
+        td6.innerHTML = "delete"
+
+        td6.addEventListener("click",()=>handledelete(i))
+
+        tr.append(td1,td2,td3,td4,td5,td6);
+
+        td6.setAttribute("class","design")
 
         document.getElementById("tbody").append(tr)
     })
 }
 
-
-const handleData = (e) => {
+const HanbleData = (e) => {
     e.preventDefault();
 
     let Data = {
-        UserName: document.getElementById("UserName").value
+        UserName : document.getElementById("UserName").value,
+        email : document.getElementById("email").value,
+        Dob : document.getElementById("Dob").value,
+        City : document.getElementById("City").value,
+        Password : document.getElementById("Password").value,
     }
 
-    array.push(Data)
+    Array.push(Data);
 
     Ui();
-
+}
+const DeleteAll =() =>{
+    document.getElementById("tbody").innerHTML = " "
+    Array = []
 }
 
-console.log(array);
-
-document.getElementById("collectData").addEventListener("submit",handleData);
+document.getElementById("CollectData").addEventListener("submit",HanbleData);
+document.getElementById("DeleteAll").addEventListener("click",DeleteAll);
---
+++ EXAM/index.js
@@ -0,0 +1,102 @@
+let User = [];
+
+const Delete =(index) =>{
+
+    User.splice(index,1)
+    Uimake();
+}
+let num =0;
+
+const Uimake = () => {
+    document.getElementById("tbody").innerHTML = " ";
+    
+    
+    User.map((ele,i)=>{
+
+        let tr = document.createElement("tr");
+
+        let td1  = document.createElement("td");
+        td1.innerHTML = ele.name;
+
+        let td2  = document.createElement("td");
+        td2.innerHTML = ele.Job
+
+        let td3  = document.createElement("td");
+        td3.innerHTML = ele.Depa
+
+        let td4  = document.createElement("td");
+        td4.innerHTML = ele.salary
+
+        let td5  = document.createElement("td");
+        td5.innerHTML = ele.Email
+
+        let td6  = document.createElement("td");
+        td6.innerHTML = ele.Exp
+
+        let td7 = document.createElement("td");
+        td7.innerHTML = role();
+        let td8 = document.createElement("td");
+        td8.innerHTML = "Delete"
+
+        td8.addEventListener("click",()=>Delete(i))
+
+        tr.append(td1,td2,td3,td4,td5,td6,td7,td8);
+
+        document.getElementById("tbody").append(tr)
+
+        td8.setAttribute("id","red");
+
+    })
+    User.map((ele)=>{
+        num = num + ele.name
+    })
+
+
+}
+console.log(num);
+const role = (Exp) => {
+    if(Exp > 5){
+        console.log("Senior");
+    }
+    else
+    {
+        console.log("junior");
+    }
+}
+
+
+const handleData = (e) => {
+    e.preventDefault();
+
+    let Data = {
+        name : document.getElementById("name").value,
+        Job : document.getElementById("Job").value,
+        Depa : document.getElementById("Depa").value,
+        salary : document.getElementById("salary").value,
+        Exp : document.getElementById("Exp").value,
+        Email : document.getElementById("Email").value,
+    }
+
+    User.push(Data);
+
+    Uimake();
+}
+
+document.getElementById("Role").addEventListener("submit",()=>{
+    if(Exp > 5){
+        console.log("senior");
+    }
+    else{
+        console.log("junior");
+    }
+})
+
+
+
+// document.getElementById("div").addEventListener("submit",show)
+
+document.getElementById("collect").addEventListener("submit",handleData)
+document.getElementById("FireAll").addEventListener("click",()=>{
+    document.getElementById("tbody").innerHTML = " ";
+    User = [];
+})
