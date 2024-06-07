e65a17ba7bfc6ff4d5e3abdd553d900049b2f4f1
keyurjivani <keyurjivani07rw@gmail.com>
2024-06-07 09:39:30 +0530

    ..

---
+++ 6-6 Storage Assignment/script.js
@@ -0,0 +1,29 @@
+let count = 0 ;
+const btn = () =>{
+count++
+document.getElementById("h2").innerText = count;
+}
+document.getElementById("btn").addEventListener("click",btn)
+
+
+let count1 = 0 ;
+const btn1 = () =>{
+count1++
+document.getElementById("h3").innerText = count1;
+}
+document.getElementById("btn1").addEventListener("click",btn1)
+
+let count2 = 0 ;
+const btn2 = () =>{
+count2++
+document.getElementById("h4").innerText = count2;
+}
+document.getElementById("btn2").addEventListener("click",btn2)
+
+
+let count3 = 0 ;
+const btn3 = () =>{
+count3++
+document.getElementById("h5").innerText = count3;
+}
+document.getElementById("btn3").addEventListener("click",btn3)
