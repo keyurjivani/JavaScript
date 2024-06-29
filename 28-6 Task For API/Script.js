52178388f33828662a1e28f1daa5284077f5941e
keyurjivani <keyurjivani07rw@gmail.com>
2024-06-29 13:46:13 +0530

    API

--- 28-6 Task For API/Script.js
+++ 28-6 Task For API/Script.js
@@ -11,33 +11,46 @@ const Ui = (data) =>{
         let Title = document.createElement("h5");
         // Title.innerHTML = `Title :  ${ele.title}`;
         Title.setAttribute("class","Title")
-        console.log(ele.title.length);
+        // console.log(ele.title.length);
         if(ele.title.length>25){
-            console.log("string");
+            // console.log("string");
             let str = "";
             for (let i = 0; i < 25; i++) {
                 str += ele.title[i];
             }
             str+="..."
-            console.log("str",str);
+            // console.log("str",str);
         Title.innerHTML = `  ${str}`;
         }else{
-            console.log("else part");
+            // console.log("else part");
         Title.innerHTML = `  ${ele.title}`;
 
         }
 
 
         let Price  = document.createElement("h4");
-        Price.innerHTML = `Price : ${ele.price}`;
+        Price.innerHTML = `Rs : ${ele.price}$`;
         Price.setAttribute("class","Price")
 
         let Category = document.createElement("p");
         Category.innerHTML = `Category : ${ele.category}`;
         Category.setAttribute("class","category")
 
-        // let Des = document.createElement("p");
-        // Des.innerHTML = ele.description
+        let Des = document.createElement("p");
+        console.log(ele.description.length);
+        if(ele.description.length > 60){
+            let string =""
+            for (let i = 0; i < 60; i++) {
+                string += ele.description[i]                
+            }
+            string+="..."
+        Des.innerHTML = string
+
+        }else{
+            Des.innerHTML = ele.description
+
+        }
+        Des.setAttribute("class","Des")
 
         let div2 = document.createElement("div");
 
@@ -55,17 +68,23 @@ const Ui = (data) =>{
 
         let btn1 = document.createElement("button");
         btn1.innerHTML = "Buy Now"
+        btn1.addEventListener("click",()=>{
+            alert("Thanks For Visite")
+        })
         btn1.setAttribute("id","btn1")
 
-        let btn2 = document.createElement("button");
-        btn2.innerHTML = "Delete"
-        btn2.setAttribute("id","btn2")
+        // let btn2 = document.createElement("button");
+        // btn2.innerHTML = "Delete"
+        // btn2.setAttribute("id","btn2")
+        // btn2.addEventListener("click",()=>{
+        //     alert("")
+        // })
 
-        div3.append(btn1,btn2)
+        div3.append(btn1)
         div3.setAttribute("id","div3")
 
         let div = document.createElement("div");
-        div.append(image,Title,Price,Category,div2,div3)
+        div.append(image,Title,Price,Category,div2,Des,div3)
         div.setAttribute("id","div_main")
 
         document.getElementById("Display").append(div)
