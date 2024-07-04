6b28cdd73954e3080fa7b021d3a2513eb26bd0ce
keyurjivani <keyurjivani07rw@gmail.com>
2024-07-04 19:03:48 +0530

    weather

--- Wheather/script.js
+++ Wheather/script.js
@@ -33,8 +33,10 @@ const mapper = (data) =>{
 
     let name = document.createElement("h2");
     name.innerHTML = data.name
+    name.style.fontSize ="30px"
     let country = document.createElement("h2");
     country.innerHTML = ` , ${data.sys.country}`
+    country.style.fontSize = "30px"
     let div1 = document.createElement("div");
     div1.append(name,country)
     div1.setAttribute("id","div1")
@@ -43,18 +45,26 @@ const mapper = (data) =>{
 
 
     let temp = document.createElement("h1");
-    temp.innerHTML = data.main.temp
+    temp.innerHTML = Math.round(data.main.temp)  
+    temp.style.fontSize = "70px"
+    temp.style.paddingBottom = "50px"
+    let p = document.createElement("p")
+    p.innerHTML = " °C"
+    p.style.fontSize = "45px"
+    
 
     let div2 = document.createElement("div");
-    div2.append(temp)
+    div2.append(temp,p)
     div2.setAttribute("id","div2")
 
 
 
     let humidity = document.createElement("p");
     humidity.innerHTML = `Humidity : ${data.main.humidity}`
+    humidity.style.fontSize = "20px"
     let pressure = document.createElement("p");
     pressure.innerHTML = `Pressure : ${data.main.pressure}`
+    pressure.style.fontSize = "20px"
     let div3 = document.createElement("div");
     div3.append(humidity,pressure)
     div3.setAttribute("id","div3")
@@ -62,7 +72,7 @@ const mapper = (data) =>{
    
 
     let div3_1 = document.createElement("div");
-    div3_1.append(div2,div3)
+    div3_1.append(div3)
     div3_1.setAttribute("id","div3_1")
 
 
@@ -71,12 +81,15 @@ const mapper = (data) =>{
 
     let visibility = document.createElement("p");
     visibility.innerHTML = `Visibility : ${data.visibility}`
+    visibility.style.fontSize = "20px"
 
     let wind = document.createElement("p");
     wind.innerHTML = `Wind Speed : ${data.wind.speed}`
+    wind.style.fontSize = "20px"
 
      let timezone = document.createElement("p");
      timezone.innerHTML = `Timezone : ${data.timezone}`
+     timezone.style.fontSize = "20px"
 
      let div4 = document.createElement("div");
      div4.append(visibility,wind,timezone)
@@ -91,6 +104,7 @@ const mapper = (data) =>{
 
      let type = document.createElement("p");
      type.innerHTML = data.weather[0].main
+     type.style.fontSize = "20px"
      
      let div6 = document.createElement("div")
      div6.append(type)
@@ -99,20 +113,24 @@ const mapper = (data) =>{
 
      let temp_min = document.createElement("p")
      temp_min.innerHTML = data.main.temp_min
+     temp_min.style.fontSize = "20px"
 
      let temp_max = document.createElement("p");
      temp_max.innerHTML = data.main.temp_max
+     temp_max.style.fontSize = "20px"
 
      let slas = document.createElement("p")
      slas.innerHTML = " / "
      slas.style.marginLeft = "6px"
      slas.style.marginRight = "6px"
+     slas.style.fontSize = "20px"
+
 
      let div5 = document.createElement("div");
      div5.append(temp_min, slas ,temp_max)
      div5.setAttribute("id","div5")
 
-    document.getElementById("Show_Wheather").append(div1,div_child,div6,div5)
+    document.getElementById("Show_Wheather").append(div1,div2,div_child,div6,div5)
 }
 
 
