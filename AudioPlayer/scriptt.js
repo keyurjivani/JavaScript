6168651a4396e7c221be550a3fc8a600d1772254
keyurjivani <keyurjivani07rw@gmail.com>
2024-07-16 16:40:56 +0530

    commit

--- AudioPlayer/scriptt.js
+++ AudioPlayer/scriptt.js
@@ -1,106 +1,105 @@
-const Audio1 = new Audio('./Music/ANIMAL_ ARJAN VAILLY _ Ranbir Kapoor _ Sandeep Vanga _ Bhupinder B_ Manan B _ Bhushan K(MP3_160K).mp3');
-const Audio2 = new Audio('./Music/Dekhha Tenu _ Mr. _ Mrs. Mahi _ Rajkummar Rao_ Janhvi Kapoor _ Mohd. Faiz _ Jaani _ Aadesh S_ Sameer(MP3_160K).mp3');
-const Audio3 = new Audio('./Music/Laal Peeli Akhiyaan (Song) Shahid Kapoor_Kriti Sanon_Tanishk_Romy _ Teri Baaton Mein Aisa Uljha Jiya(MP3_160K).mp3');
-const Audio4 = new Audio('./Music/PAISA HAI TOH - OFFICIAL LO-FI SONG _ ( SLOWED   REVERBED ) _ FARZI _ SHAHID KAPOOR _ RASHI KHANNA _(MP3_160K).mp3');
-const Audio5 = new Audio('./Music/Ruaan Full Song _ Tiger 3 _ Salman Khan_ Katrina Kaif _ Pritam_ Arijit Singh_ Irshad Kamil_ New Song(MP3_160K).mp3');
-const Audio6 = new Audio('./Music/Saanware - Akhil Sachdeva _ Abhishek Kumar _ Mannara Chopra _ Anshul Garg(MP3_160K).mp3');
-const Audio7 = new Audio('./Music/Sajni (Song)_ Arijit Singh_ Ram Sampath _ Laapataa Ladies _  Aamir Khan Productions(MP3_160K).mp3');
-const Audio8 = new Audio('./Music/SUNIYAN SUNIYAN (Official Video) Juss x MixSingh(MP3_160K).mp3');
-const Audio9 = new Audio('./Music/Tum Se (Full Video)_ Shahid Kapoor_ Kriti _ Sachin-Jigar_ Raghav Chaitanya_ Varun Jain_ Indraneel(MP3_160K).mp3');
-const Audio10 = new Audio('./Music/YODHA_ Zindagi Tere Naam (Song) _ Sidharth Malhotra_ Raashii Khanna _ Vishal Mishra(MP3_160K).mp3');
+let arr = [
+  {   id : 1,
+      Music_name : "Maar udi ",
+      img : "https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3.ap-south-1.amazonaws.com/wynk-music-cms/srch_timesmusic/2024-07/01/11-36/8902633356702.jpg",
+      Music_url : "./music/Amar Notun Gane Ei (Saraswati Puja Special Bengali Adhunik Songs All Tims Hits 2024) Dj BM Music Center-BarmanMusic.Com.mp3"
+  },
+  {
+      id : 2,
+      Music_name : "Saare Ki ",
+      img : "https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3.ap-south-1.amazonaws.com/wynk-music-cms/srch_timesmusic/2024-07/01/11-36/8902633356702.jpg",
+      Music_url : "./music/Ami Pushparaj Tui Srivalli (Purulia New Album Exited Roadshow Dance Humming Mix 2024) Dj Bm Music Center-BarmanMusic.Com.mp3"
+  },
+  {
+      id : 3,
+      Music_name : " Tere Vaaste",
+      img : "https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3-ap-south-1.amazonaws.com/wynk-music-cms/srch_hungama/music/8903431638557/1615226436/srch_hungama_24733966.jpg",
+      Music_url : "./music/Are Sare Pachtai (Purulia New Album Exited Roadshow Dance Humming Mix 2024) Dj Bm Music Center-BarmanMusic.Com.mp3"
+  },
+  {
+      id : 4,
+      Music_name : "Nashe Si Chadh Gayi ",
+      img : "https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3.ap-south-1.amazonaws.com/wynk-music-cms/srch_unisysinfo/20230817112119000/0849486075603/1692346623671/resources/0849486075603.jpg",
+      Music_url : "./music/Bagal Wala Jaan Marelu (Purulia New Album Exited Roadshow Dance Humming Mix 2024) Dj Bm Music Center-BarmanMusic.Com.mp3"
+  },
+  {
+      id : 5,
+      Music_name : " Teri Baaton Mein Aisa Uljha Jiya",
+      img : "https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3.ap-south-1.amazonaws.com/wynk-music-cms/srch_hungama/8902894362214_20240205185326/8902894362214/1707140412515/resources/8902894362214.jpg",
+      Music_url : "./music/Churi Payel (Purulia New Album Exited Roadshow Dance Humming Mix 2024) Dj Bm Music Center-BarmanMusic.Com (1).mp3"
+  },
+  {
+      id : 6,
+      Music_name : " ANIMAL (Deluxe Edition Album) ",
+      img : "https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3.ap-south-1.amazonaws.com/wynk-music-cms/srch_hungama/8903431981196_20240124153424/8903431981196/1706091605637/resources/8903431981196.jpg",
+      Music_url : "./music/Churi Payel (Purulia New Album Exited Roadshow Dance Humming Mix 2024) Dj Bm Music Center-BarmanMusic.Com.mp3"
+  },
+  {
+      id : 7,
+      Music_name : "Zaalima ",
+      img : "https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3-ap-south-1.amazonaws.com/wynk-music-cms/music/1492505048/srch_zeemusic_ZMC00357-02.jpg",
+      Music_url : "./music/Dewarba Dhodi Chatanwa (Purulia New Album Exited Roadshow Dance Humming Mix 2024) Dj Bm Music Center-BarmanMusic.Com.mp3"
+  },
+  {
+      id : 8,
+      Music_name : "Jhoome Jo Pathaan ",
+      img : "https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3.ap-south-1.amazonaws.com/wynk-music-cms/srch_unisysinfo/20230816131729000/0194548408807/1692188114616/resources/0194548408807.jpg",
+      Music_url : "./music/Gari Chalao Baby Slow Slow (Purulia New Album Exited Roadshow Dance Humming Mix 2024) Dj Bm Music Center-BarmanMusic.Com.mp3"
+  },
+  {
+      id : 9,
+      Music_name : " Dil Diyan Gallan",
+      img : "https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3-ap-south-1.amazonaws.com/wynk-music-cms/music/1513066081/srch_unisysinfo_M09050405.jpg",
+      Music_url : "./music/Jis Jivan Mein Tu Nehi Sajan (Saraswati Puja Special Hindi Running Power Music Roadshow Humming Dhamaka Mix 2024) Dj Rb Mix Kalagachia-BarmanMusic.Com.mp3"
+  },
+  {
+      id : 10,
+      Music_name : "Ghungroo ",
+      img : "https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3.ap-south-1.amazonaws.com/wynk-music-cms/srch_unisysinfo/20231003101229000/0849486006348/1696508172067/resources/0849486006348.jpg",
+      Music_url : "./music/Yaad Aa Raha Hai (Saraswati Puja Special Hindi Running Power Music Roadshow Humming Dhamaka Mix 2024) Dj Rb Mix Kalagachia-BarmanMusic.Com.mp3"
+  }
+
+]
+
+const MUSIC=(data)=>{
+
+  document.getElementById("music").innerHTML=""
+
+  let img = document.createElement("img");
+  img.src = arr[data].img;
+
+  let Music_name = document.createElement("h2");
+  Music_name.innerHTML = arr[data].Music_name;
+
+
+  let Music_url = document.createElement("audio");
+  Music_url.src = arr[data].Music_url;
+  let div = document.createElement("div");
+
+  div.append(Music_url)
+  Music_url.controls = true;
+
+  img.addEventListener("click", () => {
+      Music_url.play();
+  });
+
+  Music_name.addEventListener("click", () => {
+      Music_url.play();
+  });
 
 
 
+  // main.append(prew, stop, next)
+  let musicContainer = document.getElementById("music");
+  musicContainer.append(img, Music_name, Music_url);
+
+}
+arr.map((arr, i) =>{
   
-// selecting elements
-let prevBtn = document.querySelector('.previous');
-let playBtn = document.querySelector('.play-pause');
-let nextBtn = document.querySelector('.next');
-let songName = document.querySelector('.song-name');
-let songimg = document.querySelector('.song-img');
-let songaudio = document.querySelector('.song-audio');
-
-let playPauseIcon = document.querySelector('#play-pause-icon');
-
-
-
-let songs = [
-  { ele: Audio1, audioName: 'ARJAN VAILLY',audioimage: 'https://c.saavncdn.com/092/ANIMAL-Hindi-2023-20231124191036-500x500.jpg'},
-  { ele: Audio2, audioName: 'Dekhha Tenu ', audioimage: 'https://images.mid-day.com/images/images/2024/may/Mahi_d.jpg'} ,
-  { ele: Audio3, audioName: 'Laal Peeli Akhiyaan', audioimage: 'https://m.media-amazon.com/images/M/MV5BNjUxYTVkMGMtM2ZkNS00MzAzLTk3ZDQtMmY2NjU3ZTJkMTI1XkEyXkFqcGdeQXVyMTE0MzQwMjgz._V1_.jpg'},
-  { ele: Audio4, audioName: 'PAISA HAI TOH',audioimage: 'https://i.ytimg.com/vi/RCvgKHadHrw/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLAZuc9ydjgnfIzoWV46DCpFDSoWkQ' },
-  { ele: Audio5, audioName: 'Ruaan',audioimage: 'https://i.ytimg.com/vi/pZNYseeuV3g/maxresdefault.jpg' },
-  { ele: Audio6, audioName: 'Saanware',audioimage: 'https://i.ytimg.com/vi/wa--U0DVyRU/maxresdefault.jpg' },
-  { ele: Audio7, audioName: 'Sajni',audioimage: 'https://i.ytimg.com/vi/k3g_WjLCsXM/maxresdefault.jpg' },
-  { ele: Audio8, audioName: 'SUNIYAN SUNIYAN',audioimage: 'https://i.ytimg.com/vi/AKH6ZNSnWOA/maxresdefault.jpg' },
-  { ele: Audio9, audioName: 'Tum Se',audioimage: 'https://i.ytimg.com/vi/LpP4rtjACM8/maxresdefault.jpg' },
-  { ele: Audio10, audioName: 'YODHA_ Zindagi Tere Naam',audioimage: 'https://m.timesofindia.com/photo/107956258/size-173798/107956258.jpg' },
-
-];
-
-// console.log(songs[0].audioName);
-// let i = 0
-// songs.map((i)=>{
-//     let audioName = document.createElement('p');
-//     audioName.innerHTML = songs[i].audioName
-
-//     document.getElementById("display").append(audioName)
-// })
-
-for(const song of songs) {
-  song.ele.addEventListener('ended', ()=> {
-    updateSong('next');
-    playPauseSong();
-  })
-}
-
-let current = 0;
-let currentSong = songs[current].ele;
-songName.innerHTML = songs[current].audioName;
-
-console.log(currentSong);
-songimg.src = songs[current].audioimage;
-
-
-
-playBtn.addEventListener('click',()=> {
-  playPauseSong();
+  let songplay = document.createElement("h2");
+  songplay.innerHTML = arr.Music_name;
+  songplay.addEventListener("click", () => {
+      MUSIC(i)
+  });
+  document.getElementById("page").append(songplay);
 })
-
-nextBtn.addEventListener('click', () => {
-  updateSong('next');
-  playPauseSong();
-});
-
-prevBtn.addEventListener('click', () => {
-  updateSong('prev');
-  playPauseSong();
-});
-
-const updateSong = (action)=> {
-  currentSong.pause();
-  currentSong.currentTime = 0;
-
-  if(action === 'next'){
-    current++;
-    if(current > songs.length -1) current = 0;
-  }
-  if(action === 'prev'){
-    current--;
-    if(current < 0) current = songs.length - 1;
-  }
-  currentSong = songs[current].ele;
-  songName.textContent = songs[current].audioName;
-  songimg.src = songs[current].audioimage;
-}
-
-const playPauseSong = ()=> {
-  if(currentSong.paused){
-    currentSong.play();
-    playPauseIcon.className = 'ph-bold ph-pause';
-  }
-  else {
-    currentSong.pause();
-    playPauseIcon.className = 'ph-bold ph-play';
-  }
-}
