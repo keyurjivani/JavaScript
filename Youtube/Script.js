54a7ce02bf3014be91e37989a671f122e7e2987d
keyurjivani <keyurjivani07rw@gmail.com>
2024-07-17 21:39:46 +0530

    ..

--- Youtube/Script.js
+++ Youtube/Script.js
@@ -1,34 +1,34 @@
 
 
-// const Ui =(data) =>{
-//     console.log("data",data);
+const Ui =(data) =>{
+    console.log("data",data);
 
-//   data.map((ele)=>{
-//     let p = document.createElement('p');
-//     p.innerHTML = ele.snippet.title;
+  data.map((ele)=>{
+    let p = document.createElement('p');
+    p.innerHTML = ele.snippet.title;
 
-//     // let video = document.createElement('video');
-//     // video.src = ele.id.video_id;
-//     // video.controls = true;
-//     // p.addEventListener('click',()=>{
-//     //     video.play();
-//     // })
+    let img = document.createElement('img');
+    img.src = ele.snippet.thumbnails.default.url;
 
-//     let description = document.createElement('p');
-//     description.innerHTML = ele.snippet.description;
+    let description = document.createElement('p');
+    description.innerHTML = ele.snippet.description;
 
-//     document.getElementById('Display').append(p,description)
-//   })
-// }
+    let div = document.createElement('div');
+    div.append(img,p,description);
+    div.setAttribute('id', 'div');
+
+    document.getElementById('Display').append(div)
+  })
+}
 
 
 
-const getVideo = async() =>{
-    let req = await fetch (`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=js&type=video&key=AIzaSyAcLKQ1lIfIvxdEb0e0tWc4TkM9ESwCHlE`)
+const getVideo = async(data) =>{
+    let req = await fetch (`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=${data}&type=video&key=AIzaSyCGd4IaUr_sf2BoMMSwX-XyX2kKSLOvKMw`)
     let res = await req.json();
     console.log(res.items);
-    // Ui(res.items)
+    Ui(res.items)
     
 }
-getVideo()
+getVideo("js")
 
